bdd3fa4ff1db290020e4190d9de594f7f8ca9bf8
doughkit <siraxolot239@gmail.com>
2026-02-13 15:19:03 -0500

    when that scope creep hit

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,19 +1,42 @@
-import { VentoPlugin } from "eleventy-plugin-vento";
-import EleventySass from "@11tyrocks/eleventy-plugin-sass-lightningcss";
+import pug from "pug";
+import eleventyPluginPhosphoricons from "eleventy-plugin-phosphoricons";
+import fontAwesomePlugin from "@11ty/font-awesome";
+import yaml from "js-yaml";
 
 export default function (cfg) {
-  cfg.setInputDirectory("src");
-  cfg.setIncludesDirectory("../_includes");
-  cfg.setDataDirectory("../_data");
-  cfg.setLayoutsDirectory("../_includes/layouts");
+	cfg.setDataFileSuffixes([".dat", ""]);
 
-  cfg.addTemplateFormats("scss");
-  cfg.addTemplateFormats("css");
+	cfg.addPlugin(eleventyPluginPhosphoricons, {
+		fill: "currentColor",
+	});
+	cfg.addPlugin(fontAwesomePlugin, {
+		shortcode: "fa",
+		defaultAttributes: {
+			class: "size-6",
+		},
+	});
 
-  cfg.addPassthroughCopy("assets");
+	cfg.addDataExtension("yaml", (contents) => yaml.load(contents));
 
-  cfg.addPlugin(EleventySass);
-  cfg.addPlugin(VentoPlugin, { autotrim: true });
+	cfg.addPassthroughCopy("src/assets");
 
-  cfg.addGlobalData("domain", "https://doughkit.dev/");
+	cfg.addWatchTarget("../styles/tailwind.config.js");
+	cfg.addWatchTarget("../styles/tailwind.css");
+	cfg.addWatchTarget("pages");
+
+	cfg.addPairedShortcode("pug", (text) => {
+		return pug.render(text);
+	});
+
+	return {
+		dir: {
+			input: "src",
+			includes: "includes",
+			layouts: "layouts",
+			data: "data",
+		},
+		markdownTemplateEngine: "njk",
+		dataTemplateEngine: "njk",
+		htmlTemplateEngine: "njk",
+	};
 }
